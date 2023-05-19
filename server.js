b80a39f7601afe3cf8c54bc273857ef9fde37b90
Gopal Sasmal <gopalsasmal2000@gmail.com>
2023-05-19 23:06:02 +0530

    first commite

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const client = require("mongodb").MongoClient;
+const Objid = require("mongodb").ObjectId;
+
+//(err,database)
+//catch,then
+let dbinstance;
+///let studentInstance;
+client.connect("mongodb://0.0.0.0:27017").then((database) => {
+  dbinstance = database.db("BatchMongodb");
+  // studentInstance= dbinstance.collection("Students")
+  //   console.log(dbinstance);
+});
+
+app.get("/getData", (req, res) => {
+  dbinstance
+    .collection("Students")
+    .find({})
+    .toArray()
+    .then((result) => {
+      // console.log(result);
+      //res.end();
+      res.render("home", { data: result });
+    });
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+app.post("/storeData", (req, res) => {
+  let obj = { name: req.body.name, age: req.body.age };
+  dbinstance
+    .collection("Students")
+    .insertOne(obj)
+    .then((result) => {
+      console.log(result);
+      //res.render("home",{data:result});
+      res.redirect("/getData");
+    });
+});
+app.get("/viewData/:id", (req, res) => {
+  //console.log(req.params.id);
+  dbinstance
+    .collection("Students")
+    .findOne({ _id: new Objid(req.params.id) })
+    .then((result) => {
+      res.render("Student", { data: result });
+    });
+});
+
+app.get("/updateData/:id", (req, res) => {
+  //console.log(req.params.id);
+  dbinstance
+    .collection("Students")
+    .findOne({ _id: new Objid(req.params.id) })
+    .then((result) => {
+      res.render("UpdateStudent", { data: result });
+    });
+});
+app.post("/updateData", (req, res) => {
+  //update Students set Name=req.body.name,age=req.body.age where _id=req.body.id
+
+  dbinstance
+    .collection("Students")
+    .updateOne(
+      { _id: new Objid(req.body.id) },
+      { $set: { name: req.body.name, age: req.body.age } }
+    );
+  res.redirect("/getData");
+});
+app.listen(3000, () => {
+  console.log("listening on");
+});
